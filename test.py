49619dd798f7aa7c8723cc583d74bb07ce017986
AmirHosein-Vahed <vahed.ahv@gmail.com>
2024-02-12 12:38:38 +0330

    test for commit and rollback added. commit added after each set or lpush method in test.

--- test.py
+++ test.py
@@ -20,17 +20,20 @@ class TestPyInMemStore(unittest.TestCase):
 
     def test_set_and_get(self):
         self.db.set('foo', 'bar')
+        self.db.commit()
         result = self.db.get('foo')
         self.assertEqual(result, 'bar')
 
     def test_delete(self):
         self.db.set('del_me', 'val')
+        self.db.commit()
         self.assertTrue(self.db._key_exists('del_me'))
         self.db.delete('del_me')
         self.assertFalse(self.db._key_exists('del_me'))
 
     def test_expire(self):
         self.db.set('expired', 'val')
+        self.db.commit()
         self.db.expire('expired', 2)
         self.assertEqual(self.db.ttl('expired'), 2)
         time.sleep(3)
@@ -41,6 +44,7 @@ class TestPyInMemStore(unittest.TestCase):
 
     def test_nonexistent_key_without_expire_should_return_minus_one(self):
         self.db.set('no_ttl', 'val')
+        self.db.commit()
         self.assertEqual(-1, self.db.ttl('no_ttl'))
 
     def test_concurrent_access(self):
@@ -49,6 +53,7 @@ class TestPyInMemStore(unittest.TestCase):
         def run_thread(index):
             time.sleep(index / 10)
             self.db.set('concur', index)
+            self.db.commit()
 
         for i in range(10):
             worker = threading.Thread(target=run_thread, args=(i,))
@@ -63,10 +68,25 @@ class TestPyInMemStore(unittest.TestCase):
 
     def test_lpush_and_rpop(self):
         self.db.lpush('my_list', 1, 2, 3)
+        self.db.commit()
         first_element = self.db.rpop('my_list')
         self.assertEqual(first_element, 3)
         second_element = self.db.rpop('my_list')
         self.assertEqual(second_element, 2)
+
+    def test_commit(self):
+        self.db.set('first', '1')
+        self.db.set('second', '2')
+        self.db.commit()
+        self.assertEqual(self.db.get('first'), '1')
+        self.assertEqual(self.db.get('second'), '2')
+
+    def test_rollback(self):
+        self.db.set('first', '1')
+        self.db.set('second', '2')
+        self.db.rollback()
+        self.assertEqual(self.db.get('first'), None)
+        self.assertEqual(self.db.get('second'), None)
 
 if __name__ == '__main__':
     res = unittest.TextTestRunner().run(unittest.defaultTestLoader.loadTestsFromModule(sys.modules[__name__]))
